aa8ec9da74570b2603e12daaf846051ab026d4a2
João Pedro Abreu <jp.dellabianca@gmail.com>
2022-04-27 00:55:25 +0000

    update clean code

--- src/index.js
+++ src/index.js
@@ -2,117 +2,242 @@ const canvas = document.querySelector("canvas");
 
 const context = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const gravity = 0.5;
+canvas.width = window.innerWidth - 30;
+canvas.height = window.innerHeight - 30;
 
 class Player {
   constructor(x, y) {
     this.position = { x, y };
-    this.width = 100;
-    this.height = 100;
+    this.width = 40;
+    this.height = 40;
     this.velocity = {
       x: 0,
       y: 0,
     };
   }
-
-  draw() {
-    context.fillStyle = "red";
-    context.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-
-  update() {
-    this.draw();
-    this.position.y += this.velocity.y;
-    this.position.x += this.velocity.x;
-
-    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
-      this.velocity.y += gravity;
-    } else {
-      this.velocity.y = 0;
+  keys = {
+    right: {
+      pressed: false,
+    },
+    left: {
+      pressed: false,
+    },
+    jump: {
+      pressed: false
     }
-  }
+  };
 }
 
 class Plataform {
-  constructor() {
+  constructor(x, y) {
     this.position = {
-      x: 200,
-      y: 100,
+      x: x,
+      y: y,
     };
     this.width = 200;
     this.height = 20;
   }
+}
 
-  draw() {
-    context.fillStyle = "blue";
-    context.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+function gameFactory() {
+  const gravity = 0.5;
+  const state = {
+    players: {"player1": new Player(100, 100)},
+    plataform: {
+      "plataform1": new Plataform(200, 400),
+      "plataform2": new Plataform(230, 200),
+      "plataform3": new Plataform(250, 550)
+    }
+  }
+
+  function changeKeyPressedPlayer({key, player}) {
+    const playerId = state.players[player];
+    const keysPlayer = playerId.keys;
+    switch (key) {
+      case "a":
+        keysPlayer.left.pressed = true;
+        break;
+      case "d":
+        keysPlayer.right.pressed = true;
+        break;
+      case "w":
+        keysPlayer.jump.pressed = true;
+        break;
+    }
+   
+    
+  }
+
+  function changeKeyReleasedPlayer({key, player}) { 
+    const playerId = state.players[player];
+    const keysPlayer = playerId.keys;
+    switch (key) {
+    case "a":
+      keysPlayer.left.pressed = false;
+      break;
+    case "d":
+      keysPlayer.right.pressed = false;
+      break;
+    case "w":
+      keysPlayer.jump.pressed = false;
+      break;
+  }}
+
+  function updatePlayerPosition(player) {
+
+    if(player.position.y + player.velocity.y < 0){
+      player.position.y = 0
+      player.velocity.y += gravity;
+    } else {
+      player.position.y += player.velocity.y;
+    }
+    player.position.x += player.velocity.x;
+  
+    if (player.position.y + player.height + player.velocity.y <= canvas.height) {
+      player.velocity.y += gravity;
+    } else {
+      player.velocity.y = 0;
+    }
+  }
+
+  function isColisionPlayerAndPlataform(player, plataforms) {
+    for(const plataformId in plataforms){
+      const actualPlataform = state.plataform[plataformId]
+      if(player.position.y + player.height <= actualPlataform.position.y &&
+        player.position.y + player.height + player.velocity.y >=
+        actualPlataform.position.y &&
+        player.position.x + player.width >= actualPlataform.position.x &&
+        player.position.x <= actualPlataform.position.x + actualPlataform.width){
+          return true
+        }
+    }
+    return false;
+  }
+
+  function isColisionCanvasXRight(player) {
+    const playerPosition = player.position;
+    return(playerPosition.x + player.width >= canvas.width)
+  }
+  function isColisionCanvasXLeft(player) {
+    const playerPosition = player.position;
+    return(playerPosition.x <= 0)
+  }
+
+  function isColisionPlayerAndCanvasY(player) {
+    const playerPosition = player.position;
+    if (playerPosition.y - player.height <= 0){
+      return true
+     
+    };
+    return false
+  }
+  
+  function updatePlayerVelocity(player) {
+    const handleVelocityX = 8;
+    const jumpValue = 10;
+     
+
+    if(isColisionCanvasXRight(player)){
+      player.keys.right.pressed = false;
+    }
+    if(isColisionCanvasXLeft(player)){
+      player.keys.left.pressed = false;
+    }
+    
+    if (player.keys.right.pressed) {
+      player.velocity.x = handleVelocityX;
+    } else if (player.keys.left.pressed) {
+      player.velocity.x = -handleVelocityX;
+    } else {
+      player.velocity.x = 0;
+    }
+    
+
+    if (isColisionPlayerAndPlataform(player, state.plataform)) {
+      player.velocity.y = 0;
+    }
+    if(!isColisionPlayerAndCanvasY(player) && player.keys.jump.pressed && player.velocity.y === 0){
+      player.velocity.y = -jumpValue;
+    }
+    
+  }
+ return {changeKeyPressedPlayer,changeKeyReleasedPlayer, updatePlayerPosition, updatePlayerVelocity, state}
+
+}
+
+
+const game = gameFactory();
+
+
+function drawScreen() {
+  for(const playerId in game.state.players) {
+    const player = game.state.players[playerId]
+    context.fillStyle = "black";
+    context.fillRect(player.position.x, player.position.y, player.width, player.height);
+  }
+  for(const plataformId in game.state.plataform) {
+    const plataform = game.state.plataform[plataformId]
+    context.fillStyle = "black";
+    context.fillRect(plataform.position.x, plataform.position.y, plataform.width, plataform.height);
   }
 }
 
-const player = new Player(100, 100);
-const plataform = new Plataform();
 
-const keys = {
-  right: {
-    pressed: false,
-  },
-  left: {
-    pressed: false,
-  },
-};
+function createKeyboardDownObserver() {
+  const observerList = []
+
+  function subscribe(fn) {
+    observerList.push(fn)
+  }
+
+  function notifyAll(comand) {
+    for(const observerFunction of observerList){
+      observerFunction(comand)
+    }
+  }
+  window.addEventListener("keydown", handleKeyDown)
+  function handleKeyDown ({key}){
+    const command = {player: "player1", key}
+    notifyAll(command)
+  }
+  return subscribe;
+}
+
+function createKeyboardUpObserver() {
+  const observerList = []
+
+  function subscribe(fn) {
+    observerList.push(fn)
+  }
+
+  function notifyAll(comand) {
+    for(const observerFunction of observerList){
+      observerFunction(comand)
+    }
+  }
+  window.addEventListener("keyup", handleKeyUp)
+  function handleKeyUp ({key}){
+    const command = {player: "player1", key}
+    notifyAll(command)
+  }
+  return subscribe;
+}
+
+
+const keyboardListenerDown = createKeyboardDownObserver();
+const keyboardListenerUp = createKeyboardUpObserver()
+keyboardListenerDown(game.changeKeyPressedPlayer);
+keyboardListenerUp(game.changeKeyReleasedPlayer);
+
 
 function animate() {
   requestAnimationFrame(animate);
   context.clearRect(0, 0, canvas.width, canvas.height);
-  player.update();
-  plataform.draw();
-
-  const handleVelocityX = 0.2;
-  if (keys.right.pressed) {
-    player.velocity.x += handleVelocityX;
-  } else if (keys.left.pressed) {
-    player.velocity.x -= handleVelocityX;
-  } else {
-    player.velocity.x = 0;
-  }
-
-  if (
-    player.position.y + player.height <= plataform.position.y &&
-    player.position.y + player.height + player.velocity.y >=
-      plataform.position.y &&
-    player.position.x + player.width >= plataform.position.x &&
-    player.position.x <= plataform.position.x + plataform.width
-  ) {
-    player.velocity.y = 0;
-  }
+  game.updatePlayerVelocity(game.state.players.player1); 
+  game.updatePlayerPosition(game.state.players.player1);
+  drawScreen();
 }
 
 animate();
 
-window.addEventListener("keydown", ({ key }) => {
-  switch (key) {
-    case "a":
-      keys.left.pressed = true;
-      break;
-    case "d":
-      keys.right.pressed = true;
-      break;
-    case "w":
-      player.velocity.y -= 20;
-      break;
-  }
-});
-
-window.addEventListener("keyup", ({ key }) => {
-  switch (key) {
-    case "a":
-      keys.left.pressed = false;
-      break;
-    case "d":
-      keys.right.pressed = false;
-      break;
-  }
-});
